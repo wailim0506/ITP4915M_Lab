4b892062334d80f72f66402f8b9fd24d9cd68d47
wailim0506 <wailim0506@icloud.com>
2024-02-21 11:39:53 +0800

    add menu.cs

---
+++ ITP4915M_Lab/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915M_Lab
+{
+    public partial class Menu : Form
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Lab1 lab1 = new Lab1();
+            lab1.ShowDialog();
+        }
+    }
+}
