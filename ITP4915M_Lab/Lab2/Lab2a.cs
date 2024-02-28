27446990bd96db0618690ee759f6e5fa34aafdaf
wailim0506 <wailim0506@icloud.com>
2024-02-28 09:57:48 +0800

    Add Lab2a

---
+++ ITP4915M_Lab/Lab2/Lab2a.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ITP4915M_Lab.Lab2
+{
+    public partial class Lab2a : Form
+    {
+        public Lab2a()
+        {
+            InitializeComponent();
+        }
+    }
+}
