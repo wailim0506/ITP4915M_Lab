2b5a3e985a16cecb1141927b9ecb8e15f999548a
wailim0506 <wailim0506@icloud.com>
2024-03-06 11:46:54 +0800

    Lab3b

--- ITP4915M_Lab/Lab3/Lab3b.cs
+++ ITP4915M_Lab/Lab3/Lab3b.cs
@@ -32,16 +32,24 @@ namespace ITP4915M_Lab.Lab3
             double amount = Convert.ToDouble(txt_Amount.Text);
             double rate = Convert.ToDouble(txt_InterestRate.Text);
             double total = amount;
-
+            
             for (int i = 0; i < 10; i++) {
                 total = total * (1 + rate);
             }
-
             lb_Value10Year.Text = total.ToString();
 
             ////method 2:
             //total = amount * Math.Pow(1 + rate, 10);
             //lb_Value10Year.Text = total.ToString();
+
+            //year to reach 1M:
+            int year = 0;
+            total = amount;
+            while (total < 1000000) { 
+                total = total * (1 + rate); 
+                year++;
+            }
+            lb_YeartoReach.Text = year.ToString();
         }
     }
 }
