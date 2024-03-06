c337019b97237edab3815e99865917224a928120
wailim0506 <wailim0506@icloud.com>
2024-03-06 10:57:52 +0800

    Lab3a

---
+++ ITP4915M_Lab/Lab3/Lab3a.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ITP4915M_Lab.Lab3
+{
+    partial class Lab3a
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_Movie = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "My Choice of Best Movie Series";
+            // 
+            // comboBox_Movie
+            // 
+            this.comboBox_Movie.FormattingEnabled = true;
+            this.comboBox_Movie.Items.AddRange(new object[] {
+            "The Godfather",
+            "Terminator",
+            "Back to the Future",
+            "Matrix"});
+            this.comboBox_Movie.Location = new System.Drawing.Point(60, 52);
+            this.comboBox_Movie.Name = "comboBox_Movie";
+            this.comboBox_Movie.Size = new System.Drawing.Size(139, 21);
+            this.comboBox_Movie.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cast Your Vote";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Lab3a
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(280, 142);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox_Movie);
+            this.Controls.Add(this.label1);
+            this.Name = "Lab3a";
+            this.Text = "Lab3a";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_Movie;
+        private System.Windows.Forms.Button button1;
+    }
+}
